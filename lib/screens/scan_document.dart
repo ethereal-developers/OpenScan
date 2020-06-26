810df58a88e23c95a2e0f4368efc84a77072e068
im-vijayts <vijayts2001@gmail.com>
2020-06-26 21:13:28 +0530

    Necessary modules imported

--- lib/screens/scan_document.dart
+++ lib/screens/scan_document.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ScanDocument extends StatelessWidget {
+import 'dart:async';
+import 'dart:io';
 
+import 'package:camera/camera.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
+class ScanDocument extends StatefulWidget {
   static String route = "ScanDocument";
-  
+
+  @override
+  _ScanDocumentState createState() => _ScanDocumentState();
+}
+
+class _ScanDocumentState extends State<ScanDocument> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('ScanDocument'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Scan Document"),
+        ),
+        body: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
     );
   }
 }
