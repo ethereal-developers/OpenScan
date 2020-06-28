ec85fcdbb288bc803a2814ed70a5a868dd53af25
im-vijayts <vijayts2001@gmail.com>
2020-06-28 22:51:49 +0530

    Method to retrieve the names and no. of files present partially implemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:openscan/screens/scan_document.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:openscan/screens/view_document.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static String route = "HomeScreen";
@@ -13,6 +14,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void getDirectoryNames() async {
+    // TODO: Append values to the map
+    Map<String, int> appDetails;
+    Directory appDir = await getExternalStorageDirectory();
+    Directory appDirPath = Directory("${appDir.path}");
+    appDirPath
+        .list(recursive: false, followLinks: false)
+        .listen((FileSystemEntity entity) {
+      String path = entity.path;
+      int n;
+      var some =
+          Directory(entity.path).listSync(recursive: false, followLinks: false);
+      n = some.length;
+      // path is a string that contains the name of the folder
+      // n is the number of files present inside the folder
+      // This function is triggered when the user clicks on any of the ListTiles
+      print("$path: $n");
+    });
+    print(appDetails);
+  }
+
   List<ListTile> getDocuments({@required BuildContext context, int temp}) {
     List<ListTile> documentsList = [];
     for (int i = 0; i < temp; i++) {
@@ -22,7 +44,8 @@ class _HomeScreenState extends State<HomeScreen> {
           title: Text("Name of the document"),
           subtitle: Text("Date and size of the file"),
           trailing: Icon(Icons.arrow_right),
-          onTap: () {
+          onTap: () async {
+            getDirectoryNames();
             Navigator.pushNamed(context, ViewDocument.route);
           },
         ),
