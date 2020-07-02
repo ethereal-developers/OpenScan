442e788a4407f4e7541818e4cb43ff20889f6cdd
im-vijayts <vijayts2001@gmail.com>
2020-07-02 18:17:27 +0530

    Implemented Save PDF to device in OpenScan folder

--- lib/screens/view_document.dart
+++ lib/screens/view_document.dart
@@ -1,8 +1,15 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:directory_picker/directory_picker.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+
 import 'package:openscan/Utilities/Image_Card.dart';
 import 'package:openscan/Utilities/constants.dart';
+
 import 'package:openscan/screens/share_document.dart';
 
 class ViewDocument extends StatefulWidget {
@@ -17,7 +24,15 @@ class ViewDocument extends StatefulWidget {
 }
 
 class _ViewDocumentState extends State<ViewDocument> {
-  var imageFiles;
+  List<FileSystemEntity> imageFiles;
+
+  String dirName;
+  Directory selectedDirectory;
+
+  List<File> images = [];
+  String fileName;
+
+  bool _statusSuccess;
 
   void imageEditCallback() {
     _getImages();
@@ -27,6 +42,12 @@ class _ViewDocumentState extends State<ViewDocument> {
     setState(() {
       imageFiles = Directory(widget.dirPath)
           .listSync(recursive: false, followLinks: false);
+      images = [];
+      Directory(dirName)
+          .list(recursive: false, followLinks: false)
+          .listen((FileSystemEntity entity) {
+        images.add(File(entity.path));
+      });
     });
   }
 
@@ -34,6 +55,93 @@ class _ViewDocumentState extends State<ViewDocument> {
   void initState() {
     super.initState();
     _getImages();
+    dirName = widget.dirPath;
+    fileName =
+        dirName.substring(dirName.lastIndexOf("/") + 1, dirName.length - 1);
+  }
+
+  Future<void> _pickDirectory(BuildContext context) async {
+    Directory directory = selectedDirectory;
+    if (Platform.isAndroid) {
+      directory = Directory("/storage/emulated/0/");
+    } else {
+      directory = await getExternalStorageDirectory();
+    }
+
+    Directory newDirectory = await DirectoryPicker.pick(
+        allowFolderCreation: true,
+        context: context,
+        rootDirectory: directory,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))));
+
+    setState(() {
+      selectedDirectory = newDirectory;
+    });
+  }
+
+  Future<void> displayDialog() async {
+    String displayText;
+
+    if (_statusSuccess)
+      displayText = "Success. File stored in the OpenScan folder.";
+    else
+      displayText = "Failed to generate pdf. Try Again.";
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Alert'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('$displayText'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _createPdf() async {
+    try {
+      final output = File("${selectedDirectory.path}/$fileName.pdf");
+      print(output);
+      print(images);
+
+      int i = 0;
+
+      final doc = pw.Document();
+
+      for (i = 0; i < images.length; i++) {
+        final image = PdfImage.file(
+          doc.document,
+          bytes: images[i].readAsBytesSync(),
+        );
+
+        doc.addPage(pw.Page(build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Image(image),
+          ); // Center
+        }));
+      }
+
+      output.writeAsBytesSync(doc.save());
+      _statusSuccess = true;
+    } catch (e) {
+      _statusSuccess = false;
+    }
   }
 
   @override
@@ -136,7 +244,7 @@ class _ViewDocumentState extends State<ViewDocument> {
                 ),
                 GestureDetector(
                   child: Icon(Icons.edit),
-                  onTap: (){
+                  onTap: () {
                     // TODO: Rename folder
                   },
                 ),
@@ -152,28 +260,37 @@ class _ViewDocumentState extends State<ViewDocument> {
           ListTile(
             leading: Icon(Icons.add_a_photo),
             title: Text('Add Image'),
-            onTap: (){
-              //TODO: Call Cropper
+            onTap: () {
+              // TODO; Implement add image functionality
             },
           ),
           ListTile(
             leading: Icon(Icons.phone_android),
             title: Text('Save to device'),
-            onTap: (){
-              // TODO: Save PDF to downloads
+            onTap: () async {
+              if (Platform.isAndroid) {
+                if (!Directory("/storage/emulated/0/OpenScan").existsSync()) {
+                  Directory("/storage/emulated/0/OpenScan").createSync();
+                }
+                selectedDirectory = Directory("/storage/emulated/0/OpenScan");
+              } else {
+                await _pickDirectory(context);
+              }
+              await _createPdf();
+              displayDialog();
             },
           ),
           ListTile(
             leading: Icon(Icons.picture_as_pdf),
             title: Text('Share as PDF'),
-            onTap: (){
+            onTap: () {
               // TODO: Share
             },
           ),
           ListTile(
             leading: Icon(Icons.image),
             title: Text('Share as image'),
-            onTap: (){
+            onTap: () {
               //TODO: Share
             },
           ),
