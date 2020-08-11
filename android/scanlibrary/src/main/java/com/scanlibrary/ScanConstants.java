4885e8716037202e0d084a9edff7c669ac7dff2a
im-vijayts <38136104+veejayts@users.noreply.github.com>
2020-08-11 13:15:39 +0530

    Custom plugin implementation

---
+++ android/scanlibrary/src/main/java/com/scanlibrary/ScanConstants.java
@@ -0,0 +1,14 @@
+package com.scanlibrary;
+
+import android.os.Environment;
+
+/**
+ * Created by jhansi on 15/03/15.
+ */
+public class ScanConstants {
+    public final static int START_CAMERA_REQUEST_CODE = 2;
+    public final static String SCANNED_RESULT = "scannedResult";
+    public final static String SELECTED_BITMAP = "selectedBitmap";
+    public final static String TEMP_DIR = "AppDocDir";
+    public final static String IMAGE_PATH = "ImagePath";
+}
