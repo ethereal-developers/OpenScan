1a685e5a25c5e4209fc8b40ee05872493f91eb3e
im-vijayts <38136104+veejayts@users.noreply.github.com>
2020-08-13 11:33:24 +0530

    Added rotate image

--- android/scanlibrary/src/main/java/com/scanlibrary/ScanFragment.java
+++ android/scanlibrary/src/main/java/com/scanlibrary/ScanFragment.java
@@ -34,6 +34,7 @@ import java.util.Map;
 public class ScanFragment extends Fragment {
 
     private Button scanButton;
+    private Button rotateButton;
     private ImageView sourceImageView;
     private FrameLayout sourceFrame;
     private PolygonView polygonView;
@@ -65,7 +66,9 @@ public class ScanFragment extends Fragment {
     private void init() {
         sourceImageView = (ImageView) view.findViewById(R.id.sourceImageView);
         scanButton = (Button) view.findViewById(R.id.scanButton);
+        rotateButton = (Button) view.findViewById(R.id.rotateButton);
         scanButton.setOnClickListener(new ScanButtonClickListener());
+        rotateButton.setOnClickListener(new RotateButtonClickListener());
         sourceFrame = (FrameLayout) view.findViewById(R.id.sourceFrame);
         polygonView = (PolygonView) view.findViewById(R.id.polygonView);
         sourceFrame.post(new Runnable() {
@@ -159,6 +162,16 @@ public class ScanFragment extends Fragment {
             } else {
                 showErrorDialog();
             }
+        }
+    }
+
+    private class RotateButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            original = Bitmap.createBitmap(original, 0, 0, original.getWidth(), original.getHeight(), matrix, true);
+            setBitmap(original);
         }
     }
 
