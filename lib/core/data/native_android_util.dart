b8a9fd8ddf22b27b48bf3c4deeac5a39f7d30285
veejayts <vijayts2001@gmail.com>
2023-02-05 15:45:21 +0530

    util method for cropimage

--- lib/core/data/native_android_util.dart
+++ lib/core/data/native_android_util.dart
@@ -4,10 +4,10 @@ class NativeAndroidUtil {
   static MethodChannel _channel =
       new MethodChannel('com.ethereal.openscan/cropper');
 
-  static Future<Map> getImageSize(String path) {
+  static Future getImageSize(String path) async {
     return _channel.invokeMethod("getImageSize", {
       "path": path,
-    }) as Future<Map>;
+    });
   }
 
   static Future detectDocument(String path) {
@@ -16,11 +16,26 @@ class NativeAndroidUtil {
     });
   }
 
-  static Future<String> compress(String src, String dest, int desiredQuality) {
+  static Future compress(String src, String dest, int desiredQuality) async {
     return _channel.invokeMethod('compress', {
       "src": src,
       "dest": dest,
       "desiredQuality": desiredQuality,
-    }) as Future<String>;
+    });
+  }
+
+  static Future cropImage(String path, double tlX, double tlY, double trX,
+      double trY, double blX, double blY, double brX, double brY) async {
+    return _channel.invokeMethod('cropImage', {
+      "path": path,
+      "tl_x": "$tlX",
+      "tl_y": "$tlY",
+      "tr_x": "$trX",
+      "tr_y": "$trY",
+      "bl_x": "$blX",
+      "bl_y": "$blY",
+      "br_x": "$brX",
+      "br_y": "$brY"
+    });
   }
 }
